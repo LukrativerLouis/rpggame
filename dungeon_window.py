8e4674f472d7cc2612721b950139da41bb3c3e91
LukrativerLouis <louis.meyer.17@gmail.com>
2026-03-20 17:59:03 +0100

    Add dungeon windows

--- dungeon_window.py
+++ dungeon_window.py
@@ -7,8 +7,29 @@ class Dungeon_Window:
         self.character = character
         
     def draw(self, canvas, mouse_pos):
-        create_rectangle(canvas, MAIN_START + 100, 200, 100, 100, 2, "green")
-        pass
+
+        spacer_padding = 5
+        main_side_padding = 20
+        base_dungeon_window_size = 400
+
+        # first dungeon window
+
+        first_dungeon_window = create_rectangle(canvas, MAIN_START + main_side_padding, main_side_padding, base_dungeon_window_size, 800, 4, "green")
+
+        show_text(canvas, "Dungeon 1", first_dungeon_window.x + first_dungeon_window.width / 2, first_dungeon_window.y + main_side_padding, "white", True)
+
+        # second dungeon window 
+
+        second_dungeon_window = create_rectangle(canvas, MAIN_START + main_side_padding + base_dungeon_window_size + spacer_padding, main_side_padding, base_dungeon_window_size, 800, 4, "blue")
+
+        show_text(canvas, "Dungeon 2", second_dungeon_window.x + second_dungeon_window.width / 2, second_dungeon_window.y + main_side_padding, "white", True)
+
+        # third dungeon window
+
+        third_dungeon_window = create_rectangle(canvas, MAIN_START + main_side_padding + (base_dungeon_window_size + spacer_padding) * 2, main_side_padding, base_dungeon_window_size, 800, 4, "red")
+
+        show_text(canvas, "Dungeon 3", third_dungeon_window.x + third_dungeon_window.width / 2, third_dungeon_window.y + main_side_padding, "white", True)
+
 
     def handle_events(self, canvas, mouse_pos):
         pass
